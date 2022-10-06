40faa0be80c45ecb22dad577cf4818aea58c9d4d
NeoLittle <mornet.remy@gmail.com>
2022-10-06 16:58:02 +0200

    premières familiarisation avec entity manager

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -42,5 +44,35 @@ class SerieController extends AbstractController
 
         return $this->render('series/create.html.twig');
 
+    }
+    /**
+     * @Route("/demo", name="demo")
+     */
+    public function demo(EntityManagerInterface $entityManager):Response{
+        //creation d'une instance de l'entité
+        $serie= new Serie();
+
+        //hydrate toutes les propriétés
+        $serie->setName('Random');
+        $serie->setBackdrop('Spaaaace');
+        $serie->setPoster('ppster');
+        $serie->setDateCreated(new \DateTime());
+        $serie->setFirstAirDate(new \DateTime("- 1 year"));
+        $serie->setLastAirDate((new \DateTime('-6 month')));
+        $serie->setGenres('Science-Fiction');
+        $serie->setOverview('Kekchoz');
+        $serie->setPopularity(100.00);
+        $serie->setVote(3.2);
+        $serie->setStatus('Canceled');
+        $serie->setTmdbId(125);
+
+        dump($serie);
+        $entityManager-> persist($serie);
+        $entityManager-> flush();
+
+
+        //$entityManager = $this->getDoctrine()->getManager();
+
+        return $this->render('series/create.html.twig');
     }
 }
